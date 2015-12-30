094db5276aa687219894364f784ead90539fa3c1
Joel Parker Henderson <joel@joelparkerhenderson.com>
2015-12-29 17:40:39 -0700

    Add awscli

--- pip-install-our-favorites.sh
+++ pip-install-our-favorites.sh
@@ -1,112 +1,113 @@
-pip install altgraph     # Graph (network) package
-pip install bdist-mpkg   # Create virtualenv built distributions
-pip install bonjour-py
-pip install cycler       # Composable style cycles
-pip install funcsigs
-pip install idna
-pip install ipaddress    # IPv4/IPv6 manipulation library
-pip install macholib     # Mach-O header analysis and editing
-pip install mock
-pip install modulegraph
-pip install ndg-httpsclient
-pip install nose
-pip install numpy
-pip install passlib
-pip install pbr
-pip install pip
-pip install py2app
-pip install pyasn1
-pip install pycparser
-pip install pyOpenSSL
-pip install pyparsing
-pip install python-dateutil
-pip install pytz
-pip install scipy
-pip install setuptools
-pip install six
-pip install Twisted
-pip install vboxapi
-pip install xattr
-pip install zope.interface
+pip install -U altgraph     # Graph (network) package
+pip install -U bdist-mpkg   # Create virtualenv built distributions
+pip install -U bonjour-py
+pip install -U cycler       # Composable style cycles
+pip install -U funcsigs
+pip install -U idna
+pip install -U ipaddress    # IPv4/IPv6 manipulation library
+pip install -U macholib     # Mach-O header analysis and editing
+pip install -U mock
+pip install -U modulegraph
+pip install -U ndg-httpsclient
+pip install -U nose
+pip install -U numpy
+pip install -U passlib
+pip install -U pbr
+pip install -U pip
+pip install -U py2app
+pip install -U pyasn1
+pip install -U pycparser
+pip install -U pyOpenSSL
+pip install -U pyparsing
+pip install -U python-dateutil
+pip install -U pytz
+pip install -U scipy
+pip install -U setuptools
+pip install -U six
+pip install -U Twisted
+pip install -U vboxapi
+pip install -U xattr
+pip install -U zope.interface
 
 ## Language
 
-pip install cffi         # C-code Foreign Function Interface
-pip install cryptography # Cryptography recipes and primitives
-pip install enum34       # Python 3.4 Enum backported
+pip install -U cffi         # C-code Foreign Function Interface
+pip install -U cryptography # Cryptography recipes and primitives
+pip install -U enum34       # Python 3.4 Enum backported
 
 ## Jobs
 
-pip install joblib        # Job library for caching, parallelizing, persistence
+pip install -U joblib        # Job library for caching, parallelizing, persistence
 
 ## Command Line Interface
 
-pip install aws-shell     # Amazon Web Services (AWS) shell for interactive commands
-pip install tqdm          # Display progress bars for loops in code (prefer vs. progressbar2)
-pip install progressbar2  # Progress bar (prefer tqdm)
-pip install click         # Create command line interfaces in a composable way
-pip install pudb          # Python debugger (prefer vs. pdb)
+pip install -U awscli        # Amazon Web Services (AWS) command line interface
+pip install -U aws-shell     # Amazon Web Services (AWS) shell for interactive commands
+pip install -U tqdm          # Display progress bars for loops in code (prefer vs. progressbar2)
+pip install -U progressbar2  # Progress bar (prefer tqdm)
+pip install -U click         # Create command line interfaces in a composable way
+pip install -U pudb          # Python debugger (prefer vs. pdb)
 
 ## Serialization
 
-pip install dill          # A better "pickle" to serialize objects to disk
-pip install marshmallow   # Simplified object serialization
+pip install -U dill          # A better "pickle" to serialize objects to disk
+pip install -U marshmallow   # Simplified object serialization
 
 ## Data
 
-pip install tinydb        # Pure Python NoSQL database
-pip install schema        # Simple data validation library (prefer vs. voluptuous)
-pip install voluptuous    # Data validation library (prefer schema)
-pip install construct     # Declarative parser (and builder) for binary data
-pip install schematics    # Data structures and transformations
-pip install pandas        # Data analysis, time series, and statistics
-pip install sqlalchemy    # SQL database abstraction library
-pip install matplotlib   # Math plotting library
+pip install -U tinydb        # Pure Python NoSQL database
+pip install -U schema        # Simple data validation library (prefer vs. voluptuous)
+pip install -U voluptuous    # Data validation library (prefer schema)
+pip install -U construct     # Declarative parser (and builder) for binary data
+pip install -U schematics    # Data structures and transformations
+pip install -U pandas        # Data analysis, time series, and statistics
+pip install -U sqlalchemy    # SQL database abstraction library
+pip install -U matplotlib   # Math plotting library
 
 ## Geo
 
-pip install folium        # Visualize geographic data
-pip install geopandas     # Geographic pandas extensions
+pip install -U folium        # Visualize geographic data
+pip install -U geopandas     # Geographic pandas extensions
 
 ## Scraping
 
-pip install pomp          # Web scraping (prefer vs. scrapy)
-pip install scrapy        # Web scraping (prefer pomp)
+pip install -U pomp          # Web scraping (prefer vs. scrapy)
+pip install -U scrapy        # Web scraping (prefer pomp)
 
 ## pybojc
 
-pip install pyobjc-core
-pip install pyobjc-framework-Accounts
-pip install pyobjc-framework-AddressBook
-pip install pyobjc-framework-AppleScriptKit
-pip install pyobjc-framework-AppleScriptObjC
-pip install pyobjc-framework-Automator
-pip install pyobjc-framework-CFNetwork
-pip install pyobjc-framework-Cocoa
-pip install pyobjc-framework-Collaboration
-pip install pyobjc-framework-CoreData
-pip install pyobjc-framework-CoreLocation
-pip install pyobjc-framework-CoreText
-pip install pyobjc-framework-DictionaryServices
-pip install pyobjc-framework-EventKit
-pip install pyobjc-framework-ExceptionHandling
-pip install pyobjc-framework-FSEvents
-pip install pyobjc-framework-InputMethodKit
-pip install pyobjc-framework-InstallerPlugins
-pip install pyobjc-framework-InstantMessage
-pip install pyobjc-framework-LatentSemanticMapping
-pip install pyobjc-framework-LaunchServices
-pip install pyobjc-framework-Message
-pip install pyobjc-framework-OpenDirectory
-pip install pyobjc-framework-PreferencePanes
-pip install pyobjc-framework-PubSub
-pip install pyobjc-framework-QTKit
-pip install pyobjc-framework-Quartz
-pip install pyobjc-framework-ScreenSaver
-pip install pyobjc-framework-ScriptingBridge
-pip install pyobjc-framework-SearchKit
-pip install pyobjc-framework-ServiceManagement
-pip install pyobjc-framework-Social
-pip install pyobjc-framework-SyncServices
-pip install pyobjc-framework-SystemConfiguration
-pip install pyobjc-framework-WebKit
+pip install -U pyobjc-core
+pip install -U pyobjc-framework-Accounts
+pip install -U pyobjc-framework-AddressBook
+pip install -U pyobjc-framework-AppleScriptKit
+pip install -U pyobjc-framework-AppleScriptObjC
+pip install -U pyobjc-framework-Automator
+pip install -U pyobjc-framework-CFNetwork
+pip install -U pyobjc-framework-Cocoa
+pip install -U pyobjc-framework-Collaboration
+pip install -U pyobjc-framework-CoreData
+pip install -U pyobjc-framework-CoreLocation
+pip install -U pyobjc-framework-CoreText
+pip install -U pyobjc-framework-DictionaryServices
+pip install -U pyobjc-framework-EventKit
+pip install -U pyobjc-framework-ExceptionHandling
+pip install -U pyobjc-framework-FSEvents
+pip install -U pyobjc-framework-InputMethodKit
+pip install -U pyobjc-framework-InstallerPlugins
+pip install -U pyobjc-framework-InstantMessage
+pip install -U pyobjc-framework-LatentSemanticMapping
+pip install -U pyobjc-framework-LaunchServices
+pip install -U pyobjc-framework-Message
+pip install -U pyobjc-framework-OpenDirectory
+pip install -U pyobjc-framework-PreferencePanes
+pip install -U pyobjc-framework-PubSub
+pip install -U pyobjc-framework-QTKit
+pip install -U pyobjc-framework-Quartz
+pip install -U pyobjc-framework-ScreenSaver
+pip install -U pyobjc-framework-ScriptingBridge
+pip install -U pyobjc-framework-SearchKit
+pip install -U pyobjc-framework-ServiceManagement
+pip install -U pyobjc-framework-Social
+pip install -U pyobjc-framework-SyncServices
+pip install -U pyobjc-framework-SystemConfiguration
+pip install -U pyobjc-framework-WebKit
